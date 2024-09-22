dfe5034769c3a5539aae50bd9ad27c37f318d4d6
Philipp Dormann <philipp@philippdormann.de>
2024-09-22 12:53:40 +0200

    docs: update example

--- packages/example/usage.js
+++ packages/example/usage.js
@@ -1,6 +1,8 @@
 import { getDeviceInfoByIdentifier } from "@philippdormann/mobiledevices"
 
-console.log(getDeviceInfoByIdentifier("iPhone15,3"));
+console.log(getDeviceInfoByIdentifier("iPhone17,4"));
 console.log(getDeviceInfoByIdentifier("LG-H815"));
 console.log(getDeviceInfoByIdentifier("GM1913"));
 console.log(getDeviceInfoByIdentifier("iPhone11,8"));
+console.log(getDeviceInfoByIdentifier("Pixel 7a"));
+console.log(getDeviceInfoByIdentifier("M2102J20SG"));
